3be20c510aedd8aa7b14adacc9da8d09c928b252
Masayoshi Wada <developer@andantesoftware.com>
2015-09-22 16:52:39 +0900

    XMLを出力するよう設定を変更

--- server.rb
+++ server.rb
@@ -1,10 +1,16 @@
 require 'sinatra'
 require 'cabocha'
 
+helpers do
+  def html_safe(text)
+    Rack::Utils.escape_html(text)
+  end
+end
+
 get '/' do
   parser = CaboCha::Parser.new
   tree = parser.parse(params["q"])
-  @result = tree.toString(CaboCha::OUTPUT_RAW_SENTENCE)
+  @result = tree.toString(CaboCha::CABOCHA_FORMAT_XML)
   erb :index
 end
 
@@ -15,12 +21,12 @@ __END__
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title></title>
+  <title>API</title>
   <style>pre { font: normal 14px/20px courier; }</style>
 </head>
 <body>
-  <pre>
-<%= @result %>
-</pre>
+  <pre><code>
+<%= html_safe @result %>
+</code></pre>
 </body>
 </html>
